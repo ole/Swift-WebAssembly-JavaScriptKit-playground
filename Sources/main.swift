4432b2f286684e3f2270247ea7b616aae937c27d
Ole Begemann <ole@oleb.net>
2025-01-03 19:46:29 +0100

    Render a triangle in Swift

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,6 +12,18 @@ func run() {
 
     let canvas = document.getElementById("canvas")
     var ctx: JSValue = canvas.getContext("2d")
-    ctx.fillStyle = "#ff0000"
+    // Draw a rectangle
+    // With more than 2 arguments, we have to use this ugly call style.
+    // Reason (I think): Embedded Swift limitations
+    ctx.fillStyle = "#ff9900"
     _ = ctx.object!.fillRect.function!(this: ctx.object!, arguments: [20, 20, 200, 100])
+    // Draw a triangle
+    ctx.strokeStyle = "#ff00ff"
+    ctx.lineWidth = 4
+    _ = ctx.beginPath()
+    _ = ctx.moveTo(80, 200)
+    _ = ctx.lineTo(160, 60)
+    _ = ctx.lineTo(240, 200)
+    _ = ctx.closePath()
+    _ = ctx.stroke()
 }
