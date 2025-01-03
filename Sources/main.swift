e1271e51c6d8dbd4cdd8e4c63c4b9aa2255320c9
Ole Begemann <ole@oleb.net>
2025-01-03 19:41:50 +0100

    Draw a rectangle in Swift into an HTML canvas

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,20 +2,16 @@ import JavaScriptKit
 
 @_expose(wasm, "run")
 func run() {
-    do {
-        try runThrowing()
-    } catch {
-        log("Swift error thrown: \(error)")
-    }
-}
-
-func runThrowing() throws {
     log("Hello from Swift!")
     let document = JSObject.global.document
 
-    var divElement = document.createElement("div")
+    let containerDiv: JSValue = document.getElementById("container")
+    var divElement: JSValue = document.createElement("div")
     divElement.innerText = "Hello from Swift!"
-    _ = document.body.appendChild(divElement)
+    _ = containerDiv.appendChild(divElement)
 
-    _ = JSObject.global.alert!("Swift is running in the browser!")
+    let canvas = document.getElementById("canvas")
+    var ctx: JSValue = canvas.getContext("2d")
+    ctx.fillStyle = "#ff0000"
+    _ = ctx.object!.fillRect.function!(this: ctx.object!, arguments: [20, 20, 200, 100])
 }
