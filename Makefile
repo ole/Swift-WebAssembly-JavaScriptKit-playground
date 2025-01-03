9d0df94fed1a27ba8e3368f911aa03c64505fad5
Ole Begemann <ole@oleb.net>
2025-01-03 16:57:16 +0100

    SwiftPM-based build process

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
 TOOLCHAIN_DIR := ${HOME}/Library/Developer/Toolchains/swift-latest.xctoolchain
 
-# Build instructions adapted from Max Desiatov:
-# https://forums.swift.org/t/some-feedback-from-my-short-experience-with-swiftwasm/69605/5
-
+# Build instructions adapted from Simon Leeb:
+# https://forums.swift.org/t/web-app-with-embedded-swift-poc-demo/75486
 wasmlib:  
 	echo "Using toolchain dir: ${TOOLCHAIN_DIR}"
 	export TOOLCHAINS=$$(plutil -extract CFBundleIdentifier raw "${TOOLCHAIN_DIR}/Info.plist") && \
 	echo "Using toolchain: $${TOOLCHAINS}" && \
 	swift --version && \
-	swiftc \
-		-Osize \
-		-Xcc -fdeclspec \
-		-target wasm32-unknown-none-wasm \
-		-enable-experimental-feature Extern \
-		-enable-experimental-feature Embedded \
-		-wmo \
-		Sources/main.swift -c -o main.o
-	"${TOOLCHAIN_DIR}/usr/bin/wasm-ld" --no-entry main.o -o Web/app.wasm
+	JAVASCRIPTKIT_EXPERIMENTAL_EMBEDDED_WASM=true \
+		swift build \
+			--toolchain "${TOOLCHAIN_DIR}" \
+			--triple wasm32-unknown-none-wasm \
+			-c release \
+			--product RenderToCanvas
+	cp .build/wasm32-unknown-none-wasm/release/RenderToCanvas.wasm Web/app.wasm
+	type -p wasm-opt && wasm-opt -Os --strip-debug Web/app.wasm -o Web/app.wasm || :
+
+# Launch a local web server to test the WebAssembly module.
+serve:
+	cd Web && \
+	python3 -m http.server 8001
--- Package.swift
+++ Package.swift
@@ -3,18 +3,56 @@
 
 import PackageDescription
 
+let shouldBuildForEmbedded =
+    Context.environment["JAVASCRIPTKIT_EXPERIMENTAL_EMBEDDED_WASM"].flatMap(Bool.init) ?? false
+
+let extraDependencies: [Target.Dependency] = shouldBuildForEmbedded
+    ? [.product(name: "dlmalloc", package: "swift-dlmalloc")]
+    : []
+
+let swiftSettings: [SwiftSetting]? = if shouldBuildForEmbedded {
+    [
+        .enableExperimentalFeature("Embedded"),
+        .enableExperimentalFeature("Extern"),
+        .unsafeFlags([
+            "-Xfrontend", "-gnone",
+            "-Xfrontend", "-disable-stack-protector",
+        ]),
+    ]
+} else {
+    nil
+}
+
+let linkerSettings: [LinkerSetting]? = if shouldBuildForEmbedded {
+    [
+        .unsafeFlags([
+            "-Xclang-linker", "-nostdlib",
+            "-Xlinker", "--no-entry",
+            "-Xlinker", "--export-if-defined=__main_argc_argv",
+        ]),
+    ]
+} else {
+    nil
+}
+
 let package = Package(
     name: "RenderToCanvas",
+    dependencies: [
+        .package(url: "https://github.com/swiftwasm/swift-dlmalloc", from: "0.1.0"),
+        .package(url: "https://github.com/swiftwasm/JavaScriptKit", branch: "main"),
+    ],
     targets: [
         .executableTarget(
             name: "RenderToCanvas",
+            dependencies: [
+                .product(name: "JavaScriptKit", package: "JavaScriptKit"),
+            ] + extraDependencies,
             cSettings: [
                 .unsafeFlags(["-fdeclspec"]),
             ],
-            swiftSettings: [
-                .enableExperimentalFeature("Embedded"),
-                .enableExperimentalFeature("Extern"),
-            ]
+            swiftSettings: swiftSettings,
+            linkerSettings: linkerSettings
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
